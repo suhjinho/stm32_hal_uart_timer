932ffdd5fc1012c467f2eb0484f5ff2f4778b712
suhjinho <58578254+suhjinho@users.noreply.github.com>
2021-01-26 14:53:05 +0900

    Add files via upload

---
+++ timer.c
@@ -0,0 +1,37 @@
+
+#include "timer.h"
+
+volatile uint8_t timer;
+void MX_TIM2_Init(void);
+
+void MX_TIM2_Init(void)
+{
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 9;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 4999;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+
---
+++ timer.h
@@ -0,0 +1,8 @@
+
+#include "stm32f4xx_hal.h"
+
+TIM_HandleTypeDef htim2;
+
+extern void MX_TIM2_Init(void);
+extern volatile uint8_t timer;
+
